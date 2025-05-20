2dd8c42c5272d4f8d3a90e147618e9a2cda71079
unkmonster <unkmonster@outlook.com>
2025-05-20 13:31:56 +0800

    feat(web): add login page

--- web/agents/app/login/page.js
+++ web/agents/app/login/page.js
@@ -1,51 +1,86 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input, Flex } from "antd";
 import { Card } from "antd";
 import { Typography } from "antd";
+import { Alert } from "antd";
 
 function LoginForm() {
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
+  const [err, setErr] = useState(null);
+
+  const onFinish = async (values) => {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_API_BASE + "/api/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: values.username,
+          password: values.password,
+        }),
+      }
+    );
+    const json = await res.json();
+
+    if (!res.ok) {
+      setErr(JSON.stringify(json));
+      return;
+    }
+
+    localStorage.setItem("token", json.token);
+    window.location.href = "/dashboard";
   };
 
   return (
-    <Form
-      name="login"
-      initialValues={{ remember: true }}
-      style={{ maxWidth: 360 }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: "Please input your Username!" }]}
+    <>
+      {err && (
+        <Alert
+          message={err}
+          type="error"
+          showIcon
+          style={{ marginBottom: "10px" }}
+        />
+      )}
+
+      <Form
+        name="login"
+        initialValues={{ remember: true }}
+        style={{ maxWidth: 360 }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined />} placeholder="用户名" />
-      </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="用户名" />
+        </Form.Item>
 
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "Please input your Password!" }]}
-      >
-        <Input prefix={<LockOutlined />} type="password" placeholder="密码" />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input prefix={<LockOutlined />} type="password" placeholder="密码" />
+        </Form.Item>
 
-      <Form.Item>
-        <Flex justify="space-between" align="center">
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>记住我</Checkbox>
-          </Form.Item>
-          <a href="">忘记密码</a>
-        </Flex>
-      </Form.Item>
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住我</Checkbox>
+            </Form.Item>
+            <a href="">忘记密码</a>
+          </Flex>
+        </Form.Item>
 
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          登录
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
 const App = () => {
